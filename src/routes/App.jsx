04e5077b141ff35423867d91c66cab932d855857
rodrigojgq <rodrigojgq@gmail.com>
2022-06-15 19:21:50 -0400

    ✨[ADD] Not found page
    Fix styles in header and footer

--- src/routes/App.jsx
+++ src/routes/App.jsx
@@ -3,6 +3,7 @@ import Footer from '@containers/Footer';
 import Header from '@containers/Header';
 import Home from '@pages/Home';
 import CoinPage from '@pages/CoinPage';
+import NotFound from '@pages/NotFound';
 import { Routes, Route } from 'react-router-dom';
 
 function App() {
@@ -12,6 +13,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/coin/:id" element={<CoinPage />} />
+        <Route path="*" element={<NotFound />} />
       </Routes>
       <Footer />
     </>
