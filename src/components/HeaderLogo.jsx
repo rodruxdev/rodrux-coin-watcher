91d2c3bdfdec7d2b69043d1abf6035671b55a70f
rodrigojgq <rodrigojgq@gmail.com>
2022-06-15 18:37:16 -0400

    ✨[ADD] Link in coins' cards and header icon
    Add scroll to top of page.
    Change coin title adding space between name and ticker.

--- src/components/HeaderLogo.jsx
+++ src/components/HeaderLogo.jsx
@@ -1,15 +1,16 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import '@styles/components/HeaderLogo.css';
 import logo from '@images/logo-oscuro.svg';
 
 function HeaderLogo() {
   return (
-    <div className="header-logo">
+    <Link className="header-logo" to="/">
       <div className="header-logo__logo">
         <img src={`${logo}`} alt="Rodrux logo" />
       </div>
       <p>Cripto Watcher</p>
-    </div>
+    </Link>
   );
 }
 
