12f0cf8cd1b904fd93c6b448eab27108f4b696bd
rodrigojgq <rodrigojgq@gmail.com>
2022-06-20 17:56:52 -0400

    Add nullish options in empty info in API

--- src/lib/createCoinInfo.js
+++ src/lib/createCoinInfo.js
@@ -5,24 +5,26 @@ const createCoinInfo = (response) => {
   const coinId = response?.id;
   const name = response?.name;
   const ticker = response?.symbol.toUpperCase();
-  const price = response?.market_data?.current_price?.usd;
-  const image = response?.image?.thumb;
-  const description = response?.description?.en;
-  const categories = response?.categories;
-  const ath = response?.market_data?.ath?.usd;
-  const atl = response?.market_data?.atl?.usd;
-  const athPercentage = response?.market_data?.ath_change_percentage?.usd;
-  const atlPercentage = response?.market_data?.atl_change_percentage?.usd;
-  const marketCap = response?.market_data?.market_cap?.usd;
-  const volume = response?.market_data?.total_volume?.usd;
-  const highDay = response.market_data?.high_24h?.usd;
-  const lowDay = response.market_data?.low_24h?.usd;
+  const price = response?.market_data?.current_price?.usd ?? NaN;
+  const image = response?.image?.thumb ?? '';
+  const description = response?.description?.en ?? 'No description';
+  const categories = response?.categories ?? ['Cryptocurrency'];
+  const ath = response?.market_data?.ath?.usd ?? NaN;
+  const atl = response?.market_data?.atl?.usd ?? NaN;
+  const athPercentage =
+    response?.market_data?.ath_change_percentage?.usd ?? NaN;
+  const atlPercentage =
+    response?.market_data?.atl_change_percentage?.usd ?? NaN;
+  const marketCap = response?.market_data?.market_cap?.usd ?? NaN;
+  const volume = response?.market_data?.total_volume?.usd ?? NaN;
+  const highDay = response.market_data?.high_24h?.usd ?? NaN;
+  const lowDay = response.market_data?.low_24h?.usd ?? NaN;
   const priceChange = [
     response?.market_data?.price_change_percentage_1h_in_currency?.usd,
     response?.market_data?.price_change_percentage_24h_in_currency?.usd,
     response?.market_data?.price_change_percentage_7d_in_currency?.usd,
   ];
-  const coinQuantity = response.market_data?.circulating_supply;
+  const coinQuantity = response.market_data?.circulating_supply ?? NaN;
   const moreInfo = {
     ath: currencyText(ath),
     atl: currencyText(atl),
--- src/lib/createCoinsList.js
+++ src/lib/createCoinsList.js
@@ -4,19 +4,19 @@ import percentageText from './percentageText';
 const createCoinsList = (response) =>
   response.map((coinInfo) => {
     const coinId = coinInfo?.id;
-    const coinRank = coinInfo?.market_cap_rank;
+    const coinRank = coinInfo?.market_cap_rank ?? 'X';
     const name = coinInfo?.name;
-    const ticker = coinInfo?.symbol.toUpperCase();
-    const price = coinInfo?.current_price;
-    const image = coinInfo?.image;
+    const ticker = coinInfo?.symbol.toUpperCase() ?? '---';
+    const price = coinInfo?.current_price ?? NaN;
+    const image = coinInfo?.image ?? '';
     const percentageChanges = [
       coinInfo?.price_change_percentage_1h_in_currency,
       coinInfo?.price_change_percentage_24h_in_currency,
       coinInfo?.price_change_percentage_7d_in_currency,
     ].map((item) => percentageText(item));
-    const marketCap = coinInfo?.market_cap;
-    const ath = coinInfo?.ath;
-    const athPercentage = coinInfo?.ath_change_percentage;
+    const marketCap = coinInfo?.market_cap ?? NaN;
+    const ath = coinInfo?.ath ?? NaN;
+    const athPercentage = coinInfo?.ath_change_percentage ?? NaN;
     const coinTableInfo = {
       coinId,
       coinRank,
